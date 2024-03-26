33e0087028f0edc1042177f0c362c952e21ec941
talha@gmail.com <asp@LAB4-2>
2024-03-26 15:01:43 +0500

    Add project files.

---
+++ simplecrud/Models/Students.cs
@@ -0,0 +1,9 @@
+﻿namespace simplecrud.Models
+{
+    public class Students
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
