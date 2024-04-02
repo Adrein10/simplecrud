609eaca59cb39e2fb14d3fa998e5c049a5aaa123
talha@gmail.com <asp@aptech.com>
2024-04-02 14:51:49 +0500

    Models Added

---
+++ simplecrud/Models/SMSContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace simplecrud.Models
+{
+    public partial class SMSContext : DbContext
+    {
+        public SMSContext()
+        {
+        }
+
+        public SMSContext(DbContextOptions<SMSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Student");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- simplecrud/Program.cs
+++ simplecrud/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using simplecrud.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<SMSContext>(item => item.UseSqlServer(builder.Configuration.GetConnectionString("dbcs")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
